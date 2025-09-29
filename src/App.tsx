3ffcfe49d5fe69663aa8338e554b13ffed53ed55
ThomasMitchellZero <42942337+ThomasMitchellZero@users.noreply.github.com>
2025-09-29 16:32:24 -0400

    nm

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@
 // managed by AIDA
 
 import React from "react";
-import Start from "./Start";
 
 export default function App(): JSX.Element {
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <Start />
-    </div>
-  );
+  return <div style={{ height: "100vh", width: "100vw" }}></div>;
 }
